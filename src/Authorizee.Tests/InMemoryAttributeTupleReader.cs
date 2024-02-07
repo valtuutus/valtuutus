b568e30783ffb1e3ee38a8ae7f217a5c4cd7b658
Lucas Goveia <lucasgoveia.dev@gmail.com>
2024-02-07 13:14:14 -0300

    Start of test project setup

---
+++ src/Authorizee.Tests/InMemoryAttributeTupleReader.cs
@@ -0,0 +1,23 @@
+﻿using Authorizee.Core;
+using Authorizee.Core.Data;
+
+namespace Authorizee.Tests;
+
+public class InMemoryAttributeTupleReader : IAttributeReader
+{
+    private readonly AttributeTuple[] _attributesTuples;
+
+    public InMemoryAttributeTupleReader(AttributeTuple[] attributesTuples)
+    {
+        _attributesTuples = attributesTuples;
+    }
+
+    public Task<AttributeTuple?> GetAttribute(AttributeFilter filter)
+    {
+        return Task.FromResult(
+            _attributesTuples.FirstOrDefault(x =>
+                x.EntityType == filter.EntityType && x.Attribute == filter.Attribute &&
+                x.EntityId == filter.EntityId)
+        );
+    }
+}
