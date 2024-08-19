bcd051feedcd4aee9dabc9853d4a7e91a05235c9
Yan Pitangui <yanpitangui@gmail.com>
2024-08-19 10:00:52 -0300

    WIP: tests for snap token based queries

---
+++ tests/Valtuutus.Tests.Shared/BaseSnapTokenSpecs.cs
@@ -0,0 +1,88 @@
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Valtuutus.Core;
+using Valtuutus.Core.Configuration;
+using Valtuutus.Core.Data;
+using Valtuutus.Core.Engines.Check;
+using Valtuutus.Core.Schemas;
+using Valtuutus.Data;
+
+namespace Valtuutus.Tests.Shared;
+
+/// <summary>
+/// This test collection asserts that reads are correctly filtering
+/// relations/attributes for when they were deleted.
+/// </summary>
+public abstract class BaseSnapTokenSpecs : IAsyncLifetime
+{
+    protected BaseSnapTokenSpecs(IDatabaseFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+    protected IDatabaseFixture Fixture { get; }
+
+    protected abstract IValtuutusDataBuilder AddSpecificProvider(IServiceCollection services);
+
+    private ServiceProvider CreateServiceProvider()
+    {
+        var services = new ServiceCollection()
+            .AddValtuutusCore(TestsConsts.Action);
+
+        AddSpecificProvider(services)
+            .AddConcurrentQueryLimit(3);
+
+        return services.BuildServiceProvider();
+    }
+
+    private (IDataReaderProvider reader, IDataWriterProvider writer) CreateProviders()
+    {
+        var serviceProvider = CreateServiceProvider();
+        var scope = serviceProvider.CreateScope();
+        return (scope.ServiceProvider.GetRequiredService<IDataReaderProvider>(), scope.ServiceProvider.GetRequiredService<IDataWriterProvider>());
+    }
+
+    public static TheoryData<List<RelationTuple>, RelationTupleFilter, List<RelationTuple>>
+        GetRelationsAfterFirstWriteData => new()
+    {
+        {
+            [new(TestsConsts.Workspaces.Identifier, TestsConsts.Workspaces.PublicWorkspace, "owner", TestsConsts.Users.Identifier, TestsConsts.Users.Alice )],
+            new RelationTupleFilter
+            {
+                EntityType = TestsConsts.Workspaces.Identifier,
+                EntityId = TestsConsts.Workspaces.PublicWorkspace,
+                Relation = "owner",
+                SnapToken = null
+            },
+            []
+        }
+
+    };
+    [Theory]
+    [MemberData(nameof(GetRelationsAfterFirstWriteData))]
+    public async Task GetRelations_After_First_Write_All_Relations_Should_be_Retrieved(List<RelationTuple> seedRelations, RelationTupleFilter filter, List<RelationTuple> expectedRelations)
+    {
+        // arrange
+        var providers = CreateProviders();
+        var snap = await providers.writer.Write(seedRelations, [], default);
+
+        // act
+        var relations = await providers.reader.GetRelations(filter with
+        {
+            SnapToken = snap
+        }, default);
+
+        // assert
+        relations.Should().BeEquivalentTo(expectedRelations);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await Fixture.ResetDatabaseAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
