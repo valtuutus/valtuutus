81eb503ef63066d6f14dbb9ecd840695aeb5c4b6
Lucas Goveia <lucasgoveia.dev@gmail.com>
2024-08-29 17:44:25 -0300

    Minor refact, get rid of reflection code, and code cleanup

--- src/Valtuutus.Core/Schemas/Function.cs
+++ src/Valtuutus.Core/Schemas/Function.cs
@@ -21,9 +21,42 @@ public record Function
         Parameters = parameters;
         Lambda = lambda;
     }
+    
+    public IDictionary<FunctionParameter, PermissionNodeExpArgument> CreateParamToArgMap(IList<PermissionNodeExpArgument> args)
+    {
+        return Parameters
+            .Aggregate(new Dictionary<FunctionParameter, PermissionNodeExpArgument>(), (arguments, parameter) =>
+            {
+                arguments.Add(parameter, args.First(a => a.ArgOrder == parameter.ParamOrder));
+                return arguments;
+            });
+    }
 
     public bool Execute(IDictionary<string, object?> arguments)
     {
         return Lambda(arguments);
+    }
+}
+
+public static class ParamToArgMapExtensions
+{
+    public static IDictionary<string, object?> ToLambdaArgs(
+        this IDictionary<FunctionParameter, PermissionNodeExpArgument> map,
+        Func<PermissionNodeExpArgumentAttribute, object?> attrValueMapper)
+    {
+        return map.ToDictionary(
+            pair => pair.Key.ParamName,
+            pair =>
+            {
+                return pair.Value switch
+                {
+                    PermissionNodeExpArgumentAttribute arg => attrValueMapper(arg),
+                    PermissionNodeExpArgumentStringLiteral arg => arg.Value,
+                    PermissionNodeExpArgumentIntLiteral arg => arg.Value,
+                    PermissionNodeExpArgumentDecimalLiteral arg => arg.Value,
+                    _ => throw new Exception("Unsuported argument type.")
+                };
+            }
+        );
     }
 }
