148ba9a5e6a257c11585f3e3b216f246ecce8401
Yan Pitangui <yanpitangui@gmail.com>
2024-09-27 18:51:55 -0300

    Source generated constants from schema (#69)
    * WIP: Source gen from schema
    
    * Generate SchemaConsts
    
    * Updated to add namespace
    trying to make integrationtests work
    
    * FIX: file is actually being generated now
    
    * up: Testes
    
    * one more try
    
    * again
    
    * zz
    
    * cleanup
    
    ---------
    
    Co-authored-by: Lucas Goveia <lucasgoveia.dev@gmail.com>

---
+++ tests/Valtuutus.Lang.SourceGen.Tests/SourceGenSpecs.TestSchemaGen_fileName=schema2.vtt#SchemaConsts.g.verified.cs
@@ -0,0 +1,13 @@
+﻿//HintName: SchemaConsts.g.cs
+namespace Valtuutus.Lang;
+
+/// <summary>
+/// Auto-generated class to access all schema members as consts.
+/// </summary>
+public static class SchemaConstsGen
+{
+	public static class User
+	{
+		public const string Name = "user";
+	}
+}
